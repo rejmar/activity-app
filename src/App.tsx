4511731edcb986d615690482e672fd9d0c9a66fe
Marcin Rejdych <maarcinrej@gmail.com>
2022-01-06 23:25:20 +0100

    Imported Header and Content to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
+import Content from "./Content/Content";
+import Header from "./Layout/Header";
 import StyledApp from "./styles";
 
-const App: FunctionComponent = () => <StyledApp>TEST APP</StyledApp>;
+const App: FunctionComponent = () => (
+  <StyledApp>
+    <Header />
+    <Content />
+  </StyledApp>
+);
 
 export default App;
--- src/styles.tsx
+++ src/styles.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export default styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   background-color: #0000;
 `;
